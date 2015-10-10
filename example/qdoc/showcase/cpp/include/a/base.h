517bc755704c1be2ca71079971e65ad48af1ee7e
Robert Hildebrandt <robert-42@web.de>
2015-10-11 00:00:59 +0200

    added test classes

---
+++ example/qdoc/showcase/cpp/include/a/base.h
@@ -0,0 +1,16 @@
+namespace A
+{
+  class Base
+  {
+  public:
+    virtual void abstractBaseMemberFunction1() = 0;
+    virtual void abstractBaseMemberFunction1(int xyz) = 0;
+    virtual void abstractBaseMemberFunction1(int) const = 0;
+    virtual void abstractBaseMemberFunction2() = 0;
+    virtual void abstractBaseMemberFunction2(int) = 0;
+    virtual void abstractBaseMemberFunction2(int) const = 0;
+    virtual void abstractBaseMemberFunction3(int) const = 0;
+    virtual void abstractBaseMemberFunction4(int) const = 0;
+    virtual void abstractBaseMemberFunction5(int) const = 0;
+  };
+}
---
+++ example/qdoc/showcase/cpp/src/a/base.cpp
@@ -0,0 +1,190 @@
+#include <a/base.h>
+
+/*!
+\class A::Base
+
+\brief This is the Base class of the Extreme class.
+
+\inmodule showcase
+\inheaderfile a/base.h
+
+Some other text. To be able to get a feeling for text formating, you need a lot of text to get a feeling for it.
+So yeah, tadaa! Here's some text. I thnk we need a little more text. A lot more text.
+
+Maybe a new paragraph would be also nice. This filling-text doesn't have to make sense.
+It joust should be a lot of it. In this case, I prefer Quantity over Quality. That's
+usually not a good idea when writing a documentation. But for a showcase I think this should be ok.
+
+Is it already eneough filltext? I don't think so. I need more! But where can I get some ideas for a text?
+I have no idea. Maybe I should tell something about my past. Well there was an apple cake my grandma used to bake.
+Maybe I should search the recipe? It had a lot of of fruit, so I don't think it was very unhealthy.
+I think it's time of a linebreak.
+\br
+Now, that we are already talking about cokkies: Have you ever wondert, why there are some sortsof candy you can
+only buy for special holidays like christmas. While there are other sorts of cookies you can buy the whole year.
+I wonder who decided to sell some cookies only for certain holidays. Hmm, probably it wasn't a decision of a single
+person. It was was a tradicion for some cookies. But then, chockolate, coconuts and vanilla were unknown in europe
+for centuries. So the tradition must have evolved in the past few centuries.
+
+Well, I think that's eneough text for now. I probably will copy this text, even if it has no meaning for anyone.
+But hey, that's the idea of it.
+
+*/
+
+namespace A
+{
+
+/*!
+\brief This the  member function Base::abstractBaseMemberFunction1() of the Base class
+
+Detailed documentation to the \c Base::abstractBaseMemberFunction1() function
+
+Some other text. To be able to get a feeling for text formating, you need a lot of text to get a feeling for it.
+So yeah, tadaa! Here's some text. I thnk we need a little more text. A lot more text.
+
+Maybe a new paragraph would be also nice. This filling-text doesn't have to make sense.
+It joust should be a lot of it. In this case, I prefer Quantity over Quality. That's
+usually not a good idea when writing a documentation. But for a showcase I think this should be ok.
+
+*/
+void Base::abstractBaseMemberFunction1()
+{
+}
+
+/*!
+\brief This the  member function Base::abstractBaseMemberFunction1(int) of the Base class
+
+Detailed documentation to the \c Base::abstractBaseMemberFunction1(int) function with the argument \a xyz.
+
+Some other text. To be able to get a feeling for text formating, you need a lot of text to get a feeling for it.
+So yeah, tadaa! Here's some text. I thnk we need a little more text. A lot more text.
+
+Maybe a new paragraph would be also nice. This filling-text doesn't have to make sense.
+It joust should be a lot of it. In this case, I prefer Quantity over Quality. That's
+usually not a good idea when writing a documentation. But for a showcase I think this should be ok.
+*/
+void Base::abstractBaseMemberFunction1(int xyz)
+{
+}
+
+/*!
+\brief This the  member function Base::abstractBaseMemberFunction1(int) const of the Base class
+
+Detailed documentation to the \c{Base::abstractBaseMemberFunction1(int) const} function with the argument \a xyz.
+
+Some other text. To be able to get a feeling for text formating, you need a lot of text to get a feeling for it.
+So yeah, tadaa! Here's some text. I thnk we need a little more text. A lot more text.
+
+Maybe a new paragraph would be also nice. This filling-text doesn't have to make sense.
+It joust should be a lot of it. In this case, I prefer Quantity over Quality. That's
+usually not a good idea when writing a documentation. But for a showcase I think this should be ok.
+*/
+void Base::abstractBaseMemberFunction1(int xyz) const
+{
+}
+
+/*!
+\brief This the  member function Base::abstractBaseMemberFunction2() of the Base class
+
+Detailed documentation to the \c Base::abstractBaseMemberFunction2() function
+
+Some other text. To be able to get a feeling for text formating, you need a lot of text to get a feeling for it.
+So yeah, tadaa! Here's some text. I thnk we need a little more text. A lot more text.
+
+Maybe a new paragraph would be also nice. This filling-text doesn't have to make sense.
+It joust should be a lot of it. In this case, I prefer Quantity over Quality. That's
+usually not a good idea when writing a documentation. But for a showcase I think this should be ok.
+
+*/
+void Base::abstractBaseMemberFunction2()
+{
+}
+
+/*!
+\brief This the  member function Base::abstractBaseMemberFunction2(int) of the Base class
+
+Detailed documentation to the \c Base::abstractBaseMemberFunction2(int) function with the argument \a xyz.
+
+Some other text. To be able to get a feeling for text formating, you need a lot of text to get a feeling for it.
+So yeah, tadaa! Here's some text. I thnk we need a little more text. A lot more text.
+
+Maybe a new paragraph would be also nice. This filling-text doesn't have to make sense.
+It joust should be a lot of it. In this case, I prefer Quantity over Quality. That's
+usually not a good idea when writing a documentation. But for a showcase I think this should be ok.
+
+*/
+void Base::abstractBaseMemberFunction2(int xyz)
+{
+}
+
+/*!
+\brief This the  member function Base::abstractBaseMemberFunction2(int) const of the Base class
+
+Detailed documentation to the \c{Base::abstractBaseMemberFunction2(int) const} function with the argument \a xyz.
+
+Some other text. To be able to get a feeling for text formating, you need a lot of text to get a feeling for it.
+So yeah, tadaa! Here's some text. I thnk we need a little more text. A lot more text.
+
+Maybe a new paragraph would be also nice. This filling-text doesn't have to make sense.
+It joust should be a lot of it. In this case, I prefer Quantity over Quality. That's
+usually not a good idea when writing a documentation. But for a showcase I think this should be ok.
+
+*/
+void Base::abstractBaseMemberFunction2(int xyz) const
+{
+}
+
+
+/*!
+\brief This the  member function Base::abstractBaseMemberFunction3(int) const of the Base class
+
+Detailed documentation to the \c{Base::abstractBaseMemberFunction3(int) const} function with the argument \a xyz.
+
+Some other text. To be able to get a feeling for text formating, you need a lot of text to get a feeling for it.
+So yeah, tadaa! Here's some text. I thnk we need a little more text. A lot more text.
+
+Maybe a new paragraph would be also nice. This filling-text doesn't have to make sense.
+It joust should be a lot of it. In this case, I prefer Quantity over Quality. That's
+usually not a good idea when writing a documentation. But for a showcase I think this should be ok.
+
+*/
+void Base::abstractBaseMemberFunction3(int xyz) const
+{
+}
+
+/*!
+\brief This the  member function Base::abstractBaseMemberFunction4(int) const of the Base class
+
+Detailed documentation to the \c{Base::abstractBaseMemberFunction4(int) const}  function with the argument \a xyz.
+
+Some other text. To be able to get a feeling for text formating, you need a lot of text to get a feeling for it.
+So yeah, tadaa! Here's some text. I thnk we need a little more text. A lot more text.
+
+Maybe a new paragraph would be also nice. This filling-text doesn't have to make sense.
+It joust should be a lot of it. In this case, I prefer Quantity over Quality. That's
+usually not a good idea when writing a documentation. But for a showcase I think this should be ok.
+
+*/
+void Base::abstractBaseMemberFunction4(int xyz) const
+{
+}
+
+/*!
+\brief This the  member function Base::abstractBaseMemberFunction5(int) const of the Base class
+
+Detailed documentation to the \c{Base::abstractBaseMemberFunction5(int) const}  function with the argument \a xyz.
+
+Some other text. To be able to get a feeling for text formating, you need a lot of text to get a feeling for it.
+So yeah, tadaa! Here's some text. I thnk we need a little more text. A lot more text.
+
+Maybe a new paragraph would be also nice. This filling-text doesn't have to make sense.
+It joust should be a lot of it. In this case, I prefer Quantity over Quality. That's
+usually not a good idea when writing a documentation. But for a showcase I think this should be ok.
+
+*/
+void Base::abstractBaseMemberFunction5(int xyz) const
+{
+}
+
+
+}
