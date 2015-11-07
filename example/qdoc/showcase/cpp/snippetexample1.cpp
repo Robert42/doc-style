d7c759d3f12f6e293dac681f4ab6e57592779d19
Robert Hildebrandt <robert-42@web.de>
2015-11-07 13:45:35 +0100

    added more codeformatting into the showcase

---
+++ example/qdoc/showcase/cpp/snippetexample1.cpp
@@ -0,0 +1,8 @@
+Some code
+//! [Snippet A]
+void snippetA()
+{
+  // do something
+}
+//! [Snippet A]
+Some code
---
+++ example/qdoc/showcase/cpp/snippetexample2.cpp
@@ -0,0 +1,8 @@
+Some code
+//! [Snippet B]
+void snippetB()
+{
+  // do something
+}
+//! [Snippet B]
+Some code
